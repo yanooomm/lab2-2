c5d7344311bebfef40e1cda3bd51e62505cf86d4
yanooomm <115118650+yanooomm@users.noreply.github.com>
2023-01-25 19:53:27 +0500

    Add files via upload

---
+++ ListWork9.cpp
@@ -0,0 +1,20 @@
+// ListWork9 Дан односвязный список и указатель на голову списка Р1. 
+// Необходимо вывести указатель на последний элемент этого списка Рх
+
+#include "pt4.h"
+using namespace std;
+
+void Solve()
+{
+    Task("ListWork9");
+    PNode cur, next;
+    pt >> cur;
+    next = cur->Next;
+    while (next != NULL)
+    {
+        cur = next;
+        next = cur->Next;
+
+    }
+    pt << cur;
+}
